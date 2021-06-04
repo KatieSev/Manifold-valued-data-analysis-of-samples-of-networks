f094367a864adb09d5685d1c9b0606fb727381f3
Katie Severn <katie.severn@nottingham.ac.uk>
2021-06-04 14:44:06 +0100

    Adding file on what to run for each plot

---
+++ producing_plots.R
@@ -0,0 +1,29 @@
+
+#MDS and dendrogram plots
+source('MDS_plots.R')
+
+#creating means, can then be saved and plots produced in cytoscape
+source('Means.R')
+
+#interpolation plots
+source('Interpolation_plos.R')
+
+#PCA plots
+source('PCA_Euclidean.R')
+source('PCA_Squareroot.R')
+source('PCA_Procrustes.R')
+
+#PCA with regression line added
+source('Regression_Euclidean_plots.R')
+source('Regression_Squareroot_plots.R')
+source('Regression_Procrustes_plots.R')
+
+#Regression Wilks test
+
+
+#Permutation test
+
+
+#Comparing means with z-type test
+
+
